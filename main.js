49cfe811a188673985a36974552474d7fd838338
Clay Stewart <claytonmstewart@gmail.com>
2014-02-01 12:24:29 -0600

    Add basic interface for drag and drop

---
+++ main.js
@@ -0,0 +1,11 @@
+(function(){
+	$(".draggable").draggable();
+	$('.pod-box').droppable({
+		drop: function( event, ui ) {
+			window.lastBox = $(this);
+			console.log('this is the div you dropped in', $(this));
+			$('.pod-box').text('');
+			$(this).text('you dropped a new person into pod ' + +$(this).attr('id')[3]);
+		}
+	});
+})();
