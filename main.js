0a595223a10c986f8d5ec15c11ecace9816b74d5
Clay Stewart <claytonmstewart@gmail.com>
2014-02-13 16:00:02 -0600

    Move styles to stylesheet, clean up basic js func

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 (function(){
-	$(".draggable").draggable({revert: "invalid"});
-	$('.pod-box').droppable({
-		drop: function( event, ui ) {
-			window.lastBox = $(this);
-			console.log('this is the div you dropped in', $(this));
-			$('.pod-box').text('');
-			$(this).text('you dropped a new person into pod ' + +$(this).attr('id')[3]);
-		}
-	});
+  $(".draggable").draggable({revert: "invalid"});
+  $('.pod-box').droppable({
+    drop: function( event, ui ) {
+      // clear all pod boxes
+      $('.pod-box').text('');
+      // tell what pod you just drop it on
+      $(this).text( 'you dropped a new person into pod ' + (+$(this).attr('id')[3]) );
+    }
+  });
 })();
